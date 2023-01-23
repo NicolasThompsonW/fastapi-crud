ba8c2b1f5884a26025e1d4f135e33b392cb104d7
NicolasThompson <nicolasthompsonweb@gmail.com>
2023-01-23 14:37:01 -0300

    7

--- routes/user.py
+++ routes/user.py
@@ -17,6 +17,54 @@ user = APIRouter()
 key = Fernet.generate_key()
 f = Fernet(key)
 
+
+# Obtener todos los usuarios
+@user.get("/users")
+async def get_users():
+    return conexion.execute(Users.select()).fetchall()
+
+
+# Agregar usuario
+@user.post("/users")
+async def create_user(user: User):
+    new_user = dict(name=user.name, email=user.email)
+    new_user["password"] = f.encrypt(user.password.encode("utf-8"))
+    result = conexion.execute(Users.insert().values(new_user))
+
+    return new_user
+
+
+# Obtener un usuario
+@user.get("/users/{id}")
+async def get_user(id: int):
+    resultado = conexion.execute(Users.select().where(Users.c.id == id)).first()
+    return resultado
+
+
+# Eliminar
+@user.delete("/users/{id}", tags=["users"])
+async def delete_user(id: int):
+    try:
+        resultado = conexion.execute(Users.delete().where(Users.c.id == id)).first()
+        return f"Usuario {id} eliminado"
+    except:
+        return Response({"message": "usuario no encontrado"})
+
+
+# Actualizar
+@user.put("/users/{id}")
+async def put_user(id: int, user: User):
+    password = f.encrypt(user.password.encode("utf8"))
+    conexion.execute(
+        Users.update()
+        .values(name=user.name, email=user.email, password=password)
+        .where(Users.c.id == id)
+    )
+
+    return dict(id=id, user=user.name, email=user.email, password=password)
+
+
+# ----------------------------- Con POO -------------------------
 # Obtener usuarios
 @user.get("/users")
 async def get_users2():
